169dca2da33ac627bd29d1b0f77363a60e340cf0
vaaaarlos <kadu.cerq@gmail.com>
2021-05-07 23:35:57 -0300

    fix(user-service): :bug: Correct to the appropriate annotation and pathvariable usage

--- example-services/user-service/src/main/java/com/example/service/user/controller/UserController.java
+++ example-services/user-service/src/main/java/com/example/service/user/controller/UserController.java
@@ -7,6 +7,7 @@ import com.example.service.user.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -32,9 +33,9 @@ public class UserController {
     return userService.saveUser(user);
   }
 
-  @RequestMapping("{id}")
+  @GetMapping("{id}")
   @ResponseStatus(HttpStatus.OK)
-  public UserWithDepartmentResponse getUserWithDepartment(@PathVariable Long userId) {
+  public UserWithDepartmentResponse getUserWithDepartment(@PathVariable("id") Long userId) {
     log.info("Inside getUserWithDepartment of UserController");
     return userService.getUserWithDepartment(userId);
   }
